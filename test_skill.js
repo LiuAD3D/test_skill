b76f89b2bfd9a154fc61beb9c3f15c6c8380fe94
LiuAD3D <45638478+LiuAD3D@users.noreply.github.com>
2018-12-14 19:13:15 +0100

    Update test_skill.js

--- test_skill.js
+++ test_skill.js
@@ -1,46 +1,20 @@
-use strict';
-const Alexa = require('ask-sdk-v1adapter');
+"use strict";
 
-exports.handler = function(event, context, callback) {
-  const alexa = Alexa.handler(event, context);
-  alexa.registerHandlers(handlers);
-  alexa.registerV2Handlers(HelpIntentHandler); // New API functions for registering v2 request handlers
-  alexa.execute();
-};
+var Alexa = require("alexa-sdk");
 
-const handlers = {
-  'LaunchRequest': function () {
-    this.emit('SayHello');
-  },
-  'HelloWorldIntent': function () {
-    this.emit('SayHello');
-  },
-  'SayHello': function () {
-    this.response.speak('Hello World!');
+var handlers = {
+  "HelloIntent": function () {
+    this.response.speak("Hello, Codecademy"); 
     this.emit(':responseReady');
   },
-  'AMAZON.CancelIntent': function () {
-    this.response.speak('Goodbye!');
-    this.emit(':responseReady');
-  },
-  'AMAZON.StopIntent': function () {
-    this.response.speak('See you later!');
+  "LaunchRequest": function () {
+    this.response.speak("Welcome to Codecademy"); 
     this.emit(':responseReady');
   }
 };
 
-// HelpIntentHandler re-written following v2 request handler interface
-const HelpIntentHandler = {
-  canHandle({requestEnvelope}) {
-    return requestEnvelope.request.type === 'IntentRequest'
-    && requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
-  },
-  handle({responseBuilder}){
-    const speechOutput = 'This is the Hello World Sample Skill. ';
-    const reprompt = 'Say hello, to hear me speak.';
-    return responseBuilder
-      .speak(speechOutput)
-      .reprompt(reprompt)
-      .getResponse();
-  },
+exports.handler = function(event, context, callback){
+  var alexa = Alexa.handler(event, context);
+    alexa.registerHandlers(handlers);
+    alexa.execute();
 };
