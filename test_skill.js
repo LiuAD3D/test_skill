b9005a08ee70f3b227569464c9d18b90c4382195
LiuAD3D <45638478+LiuAD3D@users.noreply.github.com>
2018-12-14 19:28:37 +0100

    Update test_skill.js

--- test_skill.js
+++ test_skill.js
@@ -1,20 +1,29 @@
 "use strict";
 
+// Include the Alexa SDK
 var Alexa = require("alexa-sdk");
 
+// The handlers object tells Alexa how to handle various actions
 var handlers = {
-  "HelloIntent": function () {
-    this.response.speak("Ciao, dispensa"); 
-    this.emit(':responseReady');
-  },
-  "LaunchRequest": function () {
-    this.response.speak("ben venuto in dispensa"); 
-    this.emit(':responseReady');
-  }
+	"HelloIntent": function () {
+    //Create speech output. This is what Alexa will speak back when the user says "Ask code academy to say hello"
+		this.emit(":tell", "Hello, Codecademy");
+	},
+	//Our skill will receive a LaunchRequest when the user invokes the skill with the invocation name, but does not provide any command mapping to an intent. For example, "Open code academy"
+	"LaunchRequest": function () {
+    //Create speech output. This is what Alexa will speak back when the user says "Open code academy"
+		this.emit(":tell", "Welcome to Codecademy");
+	}
 };
 
-exports.handler = function(event, context, callback){
-  var alexa = Alexa.handler(event, context);
-    alexa.registerHandlers(handlers);
-    alexa.execute();
+// This is the function that AWS Lambda calls every time Alexa uses your skill.
+exports.handler = function(event, context, callback) {
+	// Create an instance of the Alexa library and pass it the requested command.
+	var alexa = Alexa.handler(event, context);
+
+	// Give our Alexa instance the list of handlers that we created.
+	alexa.registerHandlers(handlers);
+
+	// Start our Alexa code.
+	alexa.execute();
 };
