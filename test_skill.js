2300536d32374d006dea9cfa8cbf80c0865dca01
LiuAD3D <45638478+LiuAD3D@users.noreply.github.com>
2018-12-15 15:13:40 +0000

    Update test_skill.js

--- test_skill.js
+++ test_skill.js
@@ -9,12 +9,18 @@ var handlers = {
 	//Our skill will receive a LaunchRequest when the user invokes the skill with the invocation name, but does not provide any command mapping to an intent. For example, "Open code academy"
 	"LaunchRequest": function () {
     //Create speech output. This is what Alexa will speak back when the user says "Open code academy"
-		this.emit(":tell", "Welcome to Codecademy");
+		this.emit(":tell","benvenuto in dispensa");
 	}
 	
 		"Phrase": function () {
     //Create speech output. This is what Alexa will speak back when the user says "Ask code academy to say hello"
-	var myLanguage = this.event.request.intent.slots.oggetto.value //where 'oggetto' is the suboject of Intent
+	var myItem = this.event.request.intent.slots.oggetto.value; //where 'oggetto' is the suboject of Intent
+	
+	if (myItem == "pane") {
+      this.response.speak("Corretto pane i l'oggetto");
+    } else {
+      this.response.speak("credi " + myItem + " no");
+    }
 	
 	this.emit(':responseReady');
 		//this.emit(":tell", "Hello, Codecademy");
